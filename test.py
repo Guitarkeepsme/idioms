21d6ce50ce5c952c7a7e0cbb6772d449fff57a82
Guitarkeepsme <alexeykashurnikov@gmail.com>
2022-07-21 14:51:46 +0300

    added test function for dic

--- test.py
+++ test.py
@@ -1,98 +1,21 @@
-from bs4 import BeautifulSoup
-import unicodedata
-import requests
 import json
-import re
-# import time
-# import random
-# ['https://www.theidioms.com/on-the-blink/', 'https://www.theidioms.com/on-the-blink/', 'https://www.theidioms.com/busy-as-a-beaver/', 'https://www.theidioms.com/busy-as-a-beaver/', 'https://www.theidioms.com/red-zone/', 'https://www.theidioms.com/red-zone/', 'https://www.theidioms.com/hand-in-hand/', 'https://www.theidioms.com/hand-in-hand/', 'https://www.theidioms.com/upside-down/', 'https://www.theidioms.com/upside-down/', 'https://www.theidioms.com/make-common-cause/', 'https://www.theidioms.com/make-common-cause/', 'https://www.theidioms.com/make-common-cause/', 'https://www.theidioms.com/red-book/', 'https://www.theidioms.com/red-book/', 'https://www.theidioms.com/roots-run-deep/', 'https://www.theidioms.com/roots-run-deep/', 'https://www.theidioms.com/light-of-ones-life/', 'https://www.theidioms.com/light-of-ones-life/', 'https://www.theidioms.com/chip-off-the-old-block/', 'https://www.theidioms.com/chip-off-the-old-block/', 'https://www.theidioms.com/chip-off-the-old-block/', 'https://www.theidioms.com/bun-in-the-oven/', 'https://www.theidioms.com/fan-the-flames/', 'https://www.theidioms.com/twenty-three-skidoo/', 'https://www.theidioms.com/armed-to-the-teeth/', 'https://www.theidioms.com/cross-the-line/', 'https://www.theidioms.com/the-devil-is-beating-his-wife/', 'https://www.theidioms.com/cant-judge-a-book-by-its-cover/', 'https://www.theidioms.com/rain-cats-and-dogs/', 'https://www.theidioms.com/break-a-leg/', 'https://www.theidioms.com/bed-of-roses/']
 
-all_links = []
-iteration_count = 152
-print(f"Всего обходов: {iteration_count}")
+with open("data/idiom_info.json") as file:
+    data = json.load(file)
+# for idiom in data:
+#     if idiom is not None:
+#         return idiom
 
-for page_number in range(1, 10):
-    url = "https://www.theidioms.com/list"
-    r = requests.get(url + f"/page/{page_number}/")
-    # time.sleep(random.randrange(1, 3))
+test_dic = {"1": {"один": "one"}, "2": {"два": "two"}, "3": {"три": "three"}}
 
-    soup = BeautifulSoup(r.text, "html.parser")
-    idiom_url = soup.find('div', class_='new-list').find('a')
-    links = []
-    for idiom_url in soup.find_all('a'):  # чистим ссылки, оставляя только ссылки на фразеологизмы
-        if '-' not in idiom_url.get('href') or '#' in idiom_url.get('href'):
-            pass
-        elif "privacy-policy" in idiom_url.get('href') or "opposite-words" in idiom_url.get('href'):
-            pass
-        elif "have-designs-on" in idiom_url.get('href') or "figure-of-speech" in idiom_url.get('href'):
-            pass
-        elif "according-to" in idiom_url.get('href') or "quote-unquote" in idiom_url.get('href'):
-            pass
-        else:
-            links.append(idiom_url.get('href'))
-        # это временная мера. Позже исправлю проблему, возникающую при большом количестве примеров предложений
 
-    all_links.extend(links)
-    all_links_set = set(all_links)  # перевели список во множество, чтобы исключить повторы
-    print(all_links_set)
-    for link in all_links:
-        all_links_set.add(link)
-    idiom_content = {}
-    for link in all_links_set:
-        # r = requests.get(link)
-        r_test = requests.get("https://www.theidioms.com/make-common-cause")
-        soup = BeautifulSoup(r_test.text, "lxml")
-        idiom_info = soup.find("div", class_="article")
-        print(idiom_info)
-        meanings_soup = idiom_info.find("ul")
-        sentences_soup = idiom_info.find("ol")
-        idiom_content_tmp = []
-        idiom_content_without_tags = []
-        meanings = []
-        sentences = []
-        meanings_counter = 0
-        sentences_counter = 0
-        for phrase in meanings_soup:
-            bar = str(phrase).replace("</li>", ";")  # replace("<strong>", "**").replace("</strong>", "**")
-            with_n_bar = re.sub("<[^>]*>", " ", unicodedata.normalize("NFKD", bar))
-            meanings.append(with_n_bar.replace("   ", " ").replace("  ", " ").replace(".", ""))
-            meanings_counter += 1
-            if meanings_counter == 5:
-                break
-        for example in sentences_soup:
-            bar = str(example).replace("</li>", "")  # replace("<strong>", "**").replace("</strong>", "**")
-            with_n_bar = re.sub("<[^>]*>", " ", unicodedata.normalize("NFKD", bar))
-            sentences.append(with_n_bar.replace("   ", " ").replace("  ", " "))
-            sentences_counter += 1
-            if sentences_counter == 5:
-                break
-        meanings_str = ''.join(meanings).strip()
-        sentences_str = ''.join(sentences).strip()
-        # for i in idiom_info:
-        #     # на этом этапе необходимо распределить инфу в соответствии с тэгами
-        #     # один тэг для значений, другой для примеров
-        #     # не забыть ограничить количество оных до пяти включительно
-        #     if "Origin" in i:
-        #         break
-        #     else:
-        #         idiom_content_tmp.append(i)
-        # print(meanings)
-    #     for item in idiom_content_tmp:
-    #         bar = str(item).replace("</li>", "\n")  # replace("<strong>", "**").replace("</strong>", "**")
-    #         with_n_bar = re.sub("<[^>]*>", " ", unicodedata.normalize("NFKD", bar))
-    #         idiom_content_without_tags.append(with_n_bar.strip())
-        idiom_name = link.split(".com/")[-1].replace("/", "").replace("-", " ")
-        # print(idiom_name)
-        idiom_content[idiom_name] = {
-            "idiom_name": idiom_name,
-            "idiom_meaning": meanings_str,
-            "idiom_examples": sentences_str
-        }
-        print(idiom_content)
+def sort_dic(d):
+    for key in d:
+        inside_d = d.get(key)
+        for in_key in inside_d:
+            print(inside_d.get(in_key))
 
-        with open("data/idiom_info.json", "w") as file:
-            json.dump(idiom_content, file, indent=4, ensure_ascii=False)
-    # iteration_count -= 1
-    # print(f"Обход №{page_number} завершён, осталось обходов: {iteration_count}")
-    # if iteration_count == 0:
-    #     print("Сбор данных завершен")
+
+sort_dic(test_dic)
+
+
